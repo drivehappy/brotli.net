b7e8f4bbd0164f052f9e3cf9f5f4b2c488f4b17a
mharmer <mark.harmer@pc-doctor.com>
2017-03-13 10:11:10 -0700

    Moved to .NET 2.0, removed unused references and imports.

--- Brotli.NET/Brotli.NET/Enum/BrotliEncoderParameter.cs
+++ Brotli.NET/Brotli.NET/Enum/BrotliEncoderParameter.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Brotli
+﻿namespace Brotli
 {
     public enum BrotliEncoderParameter : int
     {
--- Brotli.NET/Brotli.NET/Implement/BrotliStream.cs
+++ Brotli.NET/Brotli.NET/Implement/BrotliStream.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
 
 namespace Brotli
 {
